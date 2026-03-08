b2b071d70e9c927f918d52e95608ba8f676c686f
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-03-08 07:28:50 +0000

    feat: implement drawing modes and cell painting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 const canvas = document.querySelector<HTMLDivElement>(".canvas")!;
 const penColorInput = document.querySelector<HTMLInputElement>("#pen-color")!;
 const canvasColorInput = document.querySelector<HTMLInputElement>("#canvas-color")!;
+const randomBtn = document.querySelector<HTMLButtonElement>(".controls__btn--random")!;
+const eraserBtn = document.querySelector<HTMLButtonElement>(".controls__btn--erase")!;
+
+type Mode = "draw" | "random" | "erase";
 
 const state = {
+  mode: "draw" as Mode,
   penColor: penColorInput.value,
   canvasColor: canvasColorInput.value,
   gridSize: 16,
@@ -21,6 +26,10 @@ function createGrid(size: number) {
   }
 }
 
+function setMode(mode: Mode) {
+  state.mode = state.mode === mode ? "draw" : mode;
+}
+
 function updatePenColor() {
   state.penColor = penColorInput.value;
 }
@@ -30,9 +39,41 @@ function updateCanvasColor() {
   canvas.style.backgroundColor = state.canvasColor;
 }
 
+function paintCell(e: PointerEvent) {
+  if (e.buttons !== 1) return;
+  if (!(e.target instanceof HTMLDivElement)) return;
+  if (!e.target.matches(".canvas__cell")) return;
+
+  const cell = e.target;
+
+  switch (state.mode) {
+    case "draw":
+      cell.style.backgroundColor = state.penColor;
+      break;
+    case "random":
+      cell.style.backgroundColor = getRandomColor();
+      break;
+    case "erase":
+      cell.style.removeProperty("background-color");
+  }
+}
+
+function getRandomColor(): string {
+  const randomNumber = Math.floor(Math.random() * 0xffffff);
+  const hexCode = `#${randomNumber.toString(16).padStart(6, "0")}`;
+
+  return hexCode;
+}
+
 function setupEvents() {
+  canvas.addEventListener("pointerdown", paintCell);
+  canvas.addEventListener("pointerover", paintCell);
+
   penColorInput.addEventListener("input", updatePenColor);
   canvasColorInput.addEventListener("input", updateCanvasColor);
+
+  randomBtn.addEventListener("click", () => setMode("random"));
+  eraserBtn.addEventListener("click", () => setMode("erase"));
 }
 
 function init() {
